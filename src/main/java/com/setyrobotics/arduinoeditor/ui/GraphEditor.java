8ef1b66473accb0fbf7e2408837ccc2035e7f1dd
Andrei Dore <andrei.dore@gmail.com>
2018-08-24 10:20:50 +0300

    Graph Editor

--- src/main/java/com/setyrobotics/arduinoeditor/ui/GraphEditor.java
+++ src/main/java/com/setyrobotics/arduinoeditor/ui/GraphEditor.java
@@ -1,15 +1,18 @@
 package com.setyrobotics.arduinoeditor.ui;
 
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.HasSize;
+import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.JavaScript;
-import com.vaadin.flow.component.html.Div;
 
 //https://vaadin.com/forum/thread/17053442
+//https://vaadin.com/docs/v10/flow/polymer-templates/tutorial-template-basic.html
+@Tag("div")
 @JavaScript("/scripts/mxClient.min.js")
-public class GraphEditor extends Div {
+public class GraphEditor extends Component implements HasSize {
 
 	public GraphEditor() {
 		setId("graph-editor");
-
 	}
 
 }
