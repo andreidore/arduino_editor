40ce3448195c5175990bc52bbb6ffac0fa7c6060
Andrei Dore <andrei.dore@gmail.com>
2018-08-30 21:17:02 +0300

    Code

--- src/main/resources/scripts/button.js
+++ src/main/resources/scripts/button.js
@@ -1,18 +1,10 @@
    var m1 = new joint.shapes.devs.Model({
         position: { x: 50, y: 50 },
         size: { width: 90, height: 90 },
-        inPorts: ['in1','in2','in3'],
         outPorts: ['out'],
         ports: {
             groups: {
-                'in': {
-                    attrs: {
-                        '.port-body': {
-                            fill: '#16A085',
-                            magnet: 'passive'
-                        }
-                    }
-                },
+               
                 'out': {
                     attrs: {
                         '.port-body': {
@@ -23,7 +15,7 @@
             }
         },
         attrs: {
-            '.label': { text: 'Model', 'ref-x': .5, 'ref-y': .2 },
+            '.label': { text: 'Button', 'ref-x': .5, 'ref-y': .2 },
             rect: { fill: '#2ECC71' }
         }
     }).addTo(graph);
